f8b822bdf3feb64831e09f7a9626653abbef49b4
jhnlem <jhnlem98@gmail.com>
2024-10-29 01:02:45 +0200

    start instructions

--- main.c
+++ main.c
@@ -54,7 +54,67 @@ enum {
 	FL_NEG = 1 << 2,
 };
 
-int main(void)
+int main(int argc, const char* argv[])
 {
-	return 0;
+	if (argc < 2) {
+		printf("lc3 [image-file1] ...\n");
+		exit(2);
+	}
+
+	for (int j = 1; j < argc; ++j) {
+		if (!read_image(argv[j])) {
+			printf("failed to lad image: %s\n", argv[j]);
+			exit(1);
+		}
+	}
+
+	signal(SIGINT, handle_interrupt);
+	disable_input_buffering();
+
+	reg[R_COND] = FL_ZRO;
+
+	enum {PC_START = 0x3000};
+	reg[R_PC] = PC_START;
+
+	int running = 1;
+	while (running) {
+		uint16_t instr = mem_read(reg[R_PC]++);
+		uint16_t op = instr >> 12;
+
+		switch (op) {
+		case OP_ADD:
+			break;
+		case OP_AND:
+			break;
+		case OP_NOT:
+			break;
+		case OP_BR:
+			break;
+		case OP_JMP:
+			break;
+		case OP_JSR:
+			break;
+		case OP_LD:
+			break;
+		case OP_LDI:
+			break;
+		case OP_LDR:
+			break;
+		case OP_LEA:
+			break;
+		case OP_ST:
+			break;
+		case OP_STI:
+			break;
+		case OP_STR:
+			break;
+		case OP_TRAP:
+			break;
+		case OP_RES:
+		case OP_RTI:
+		default:
+			break;
+		}
+	}
+	restore_input_buffering();
 }
